1f6e4c92f6d1d16f98ab5f75f607658d53abd2dc
tanajaym <evasy2002@gmail.com>
2025-01-30 15:24:11 +0300

    feat: add isLoading to app js and make skeleton on loading

--- pizza-shop/src/App.js
+++ pizza-shop/src/App.js
@@ -7,45 +7,49 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import Index from "./components/PizzaBlock";
-// import PizzaBlockSkeleton from "./components/PizzaBlock/PizzaBlockSkeleton";
+import PizzaBlockSkeleton from "./components/PizzaBlock/PizzaBlockSkeleton";
 function App() {
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch(`https://6797b1f3c2c861de0c6daede.mockapi.io/items`)
-      .then((response) => response.json())
-      .then((jsonArray) => {
-        setItems(jsonArray);
-      });
+        .then((response) => response.json())
+        .then((jsonArray) => {
+          setItems(jsonArray);
+          setIsLoading(false);
+        });
   }, []);
 
   return (
-    <div className="App">
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {items.map((obj) => (
-                <Index
-                  key={obj.id}
-                  title={obj.title}
-                  price={obj.price}
-                  image={obj.image}
-                  sizes={obj.sizes}
-                  type={obj.types}
-                />
-              ))}
+      <div className="App">
+        <div className="wrapper">
+          <Header />
+          <div className="content">
+            <div className="container">
+              <div className="content__top">
+                <Categories />
+                <Sort />
+              </div>
+              <h2 className="content__title">Все пиццы</h2>
+              <div className="content__items">
+                { isLoading
+                    ? [...new Array(6)].map((item, index) => <PizzaBlockSkeleton key={index} />)
+                    : items.map((obj) => (
+                        <Index
+                            key={obj.id}
+                            title={obj.title}
+                            price={obj.price}
+                            image={obj.image}
+                            sizes={obj.sizes}
+                            type={obj.types}
+                        />
+                    ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
